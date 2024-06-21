eac4d81b3c5f3166d48f2db8cb17dd3d6adf3e17
radum2o18 <radu.lucian.milos@gmail.com>
2024-06-21 18:05:50 +0200

    test: remove stuff

--- test.js
+++ test.js
@@ -1,6 +1,1 @@
 console.log('test.js loaded')
-console.log('ci commit')
-console.log('build infrastructureManagement')
-console.log('build infrastructureManagement 3')
-console.log('build infrastructureManagement 4')
-console.log('docs')
